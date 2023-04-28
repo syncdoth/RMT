51d83cff1b03b204274b94157545dd642bca07b3
syncdoth <choisehyun98@gmail.com>
2023-04-28 18:29:23 +0800

    update training script

--- train.sh
+++ train.sh
@@ -9,7 +9,8 @@ CUDA_VISIBLE_DEVICES=$ids python main.py \
     --warmup_steps 0 \
     --weight_decay 0 \
     --eval_steps 1000 \
-    --max_steps 100000 \
+    --max_steps -1 \
+    --num_train_epochs 5 \
     --report_to 'wandb' \
     --output_dir 'outputs' \
     --logging_steps 10 \
@@ -20,6 +21,9 @@ CUDA_VISIBLE_DEVICES=$ids python main.py \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 32 \
-    --gradient_checkpointing True
+    --gradient_checkpointing False \
+    --num_segments 2 \
+    --memory_length 10 \
+    --memory_position left --write_memory_position right
 
     # --use_lora --fp16 --train_8bit
